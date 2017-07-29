552879d0b492cbd8186c13d273ba6085b03ea6fe
hareshpatel <patelharesh00@gmail.com>
2017-07-29 23:56:24 +0530

    testQ

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'bb031cb15c13daf8c9585b2bc5d5e564',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'mage217',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 22 Jul 2017 10:24:16 +0000',
+  ),
+);
